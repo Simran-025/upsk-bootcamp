93011eb13c193f5992b7fc64d64866126d05e8df
Simran-025 <sk20258705@gmail.com>
2026-07-19 22:05:01 +0530

    fix: ignore F401 for required alembic imports

--- api/alembic/versions/31e5cdfca963_create_links_table.py
+++ api/alembic/versions/31e5cdfca963_create_links_table.py
@@ -7,8 +7,8 @@ Create Date: 2026-07-19 18:27:26.499312
 """
 from typing import Sequence, Union
 
-from alembic import op
-import sqlalchemy as sa
+from alembic import op  # noqa: F401
+import sqlalchemy as sa # noqa: F401
 
 
 # revision identifiers, used by Alembic.
--- api/alembic/versions/e4e8b185ab55_restore_unique_constraint.py
+++ api/alembic/versions/e4e8b185ab55_restore_unique_constraint.py
@@ -6,8 +6,8 @@ Create Date: 2026-07-18 23:17:05.344695
 
 """
 from typing import Sequence, Union
-from alembic import op
-import sqlalchemy as sa
+from alembic import op  # noqa: F401
+import sqlalchemy as sa # noqa: F401
 
 # revision identifiers, used by Alembic.
 revision: str = 'e4e8b185ab55'
